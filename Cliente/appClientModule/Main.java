93f8097d2fce4156ac6b29780eaffdb143fe9530
Andrés <andresgomtab@gmail.com>
2023-11-08 17:45:05 +0100

    TEW-2324-1-UO270569-7.7-Servicios: Cliente Java Ayuntamiento Gijón

--- Cliente/appClientModule/Main.java
+++ Cliente/appClientModule/Main.java
@@ -1,3 +1,8 @@
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.*;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
@@ -7,9 +12,16 @@ import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
 
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NoSuchAlgorithmException, KeyManagementException{
+/*
 		Client client = ClientBuilder.newClient();
-		WebTarget target = client.target("http://localhost:8080/gestioneitorv1_1/pisos.json");
+		WebTarget target = client.target("https://opendata.gijon.es/descargar.php?id=941&tipo=JSON");
+*/
+		
+		SSLContext sslContext = SSLContext.getInstance("TLS");
+		sslContext.init(null, new TrustManager[] { new NoOpTrustManager() }, null);
+		Client client = ClientBuilder.newBuilder().sslContext(sslContext).build();
+		WebTarget target = client.target("https://opendata.gijon.es/descargar.php?id=941&tipo=JSON");
 		String result = target.request().get().readEntity(String.class);
 
 		System.out.println(result);
@@ -22,15 +34,20 @@ public class Main {
 		System.out.println("-----------TODOS----------------");
 		System.out.println(result);
 		// Procesamos el texto JSON y lo pasamos a formato SIMPLE-JSON
-		Object obj = JSONValue.parse(result);
-		JSONArray pisos = (JSONArray) obj;
+		
+		JSONObject json = (JSONObject) JSONValue.parse(result);
+		System.out.println("JSONObject parseado: "+json);
+		JSONObject datos = ((JSONObject) json.get("datos"));
+		System.out.println("JSONObject \"datos\": "+datos);
+		JSONArray dato = ((JSONArray) datos.get("dato"));
+		System.out.println("JSONArray \"dato\": "+dato);
 		// Imprimimos el contacto tercero (2) transformándolo a formato cadena.
-		System.out.println("----------- PISO ----------------");
-		System.out.println(pisos.get(2).toString());
-		System.out.println("----------- CIUDAD DE UN PISO ----------------");
-		JSONObject unPiso = (JSONObject) JSONValue.parse(pisos.get(2).toString());
-		String ciudad = (String) unPiso.get("Ciudad");
-		System.out.println(ciudad);
+		System.out.println("----------- Equipo ----------------");
+		System.out.println(dato.get(2).toString());
+		System.out.println("----------- Ubicación de un equipo ----------------");
+		JSONObject equipo = (JSONObject) JSONValue.parse(dato.get(2).toString());
+		String ubi = (String) equipo.get("ubicacion").toString();
+		System.out.println(ubi);
 	}
 
 	/*
@@ -42,4 +59,17 @@ public class Main {
 		super();
 	}
 
+
+}
+
+class NoOpTrustManager implements X509TrustManager {
+	public X509Certificate[] getAcceptedIssuers() {
+		return null;
+	}
+
+	public void checkClientTrusted(X509Certificate[] certs, String authType) {
+	}
+
+	public void checkServerTrusted(X509Certificate[] certs, String authType) {
+	}
 }
