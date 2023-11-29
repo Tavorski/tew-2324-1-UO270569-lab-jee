485724fd2b280d9f770dded5cc73995858260e18
Andrés <andresgomtab@gmail.com>
2023-11-29 17:47:07 +0100

    TEW-2324-1-UO270569-9.2-Model: Ready de jQuery

--- gestioneitorCliv0_0/WebContent/javascript/functions_crudalumno.js
+++ gestioneitorCliv0_0/WebContent/javascript/functions_crudalumno.js
@@ -2,45 +2,51 @@
 function Model() {
 	// Lista de alumnos.
 	this.tbAlumnos = null;
-};
-// Carga los datos del servicio sobreescribiendo el dato this.tbAlumnos.
-this.load = function() {
-	this.tbAlumnos = AlumnosServicesRs.getAlumnos();
-}
-// Llamamos al servicio de alta de alumno
-this.add = function(alumno) {
-	// Llamamos al servicio de alta de alumno
-	AlumnosServicesRs.saveAlumno({
-		$entity : alumno,
-		$contentType : "application/json"
-	});
-	// Recargamos la lista de alumnos.
-	this.load();
-}
-// Actualización de un alumno existente: PENDIENTE DE IMPLEMENTAR
-this.edit = function(alumno) {
-	// Llamamos al servicio de actualizadp de alumno
-	AlumnosServicesRs.updateAlumno({
-		id : id_alumno
-	});
-	// Recargamos la lista de alumnos.
-	this.load();
-}
-// Eliminación un alumno existente
-this.remove = function(id_alumno) {
-	// Llamamos al servicio de borrado de alumno
-	AlumnosServicesRs.deleteAlumno({
-		id : id_alumno
-	});
-	// Recargamos la lista de alumnos.
-	this.load();
-}
-this.find = function(id_alumno) {
-	function checkAlumno(obj) {
-		return obj.id == id_alumno;
+	// Carga los datos del servicio sobreescribiendo el dato this.tbAlumnos.
+	this.load = function() {
+		this.tbAlumnos = AlumnosServicesRs.getAlumnos();
 	}
-	// Buscamos los datos del alumno cuyo id_alumno sea el mismo que el
-	// seleccionado
-	var alumno = this.tbAlumnos.find(checkAlumno);
-	return alumno;
-}
+	// Llamamos al servicio de alta de alumno
+	this.add = function(alumno) {
+		// Llamamos al servicio de alta de alumno
+		AlumnosServicesRs.saveAlumno({
+			$entity : alumno,
+			$contentType : "application/json"
+		});
+		// Recargamos la lista de alumnos.
+		this.load();
+	}
+	// Actualización de un alumno existente: PENDIENTE DE IMPLEMENTAR
+	this.edit = function(alumno) {
+		// Llamamos al servicio de actualizadp de alumno
+		AlumnosServicesRs.updateAlumno({
+			id : id_alumno
+		});
+		// Recargamos la lista de alumnos.
+		this.load();
+	}
+	// Eliminación un alumno existente
+	this.remove = function(id_alumno) {
+		// Llamamos al servicio de borrado de alumno
+		AlumnosServicesRs.deleteAlumno({
+			id : id_alumno
+		});
+		// Recargamos la lista de alumnos.
+		this.load();
+	}
+	this.find = function(id_alumno) {
+		function checkAlumno(obj) {
+			return obj.id == id_alumno;
+		}
+		// Buscamos los datos del alumno cuyo id_alumno sea el mismo que el
+		// seleccionado
+		var alumno = this.tbAlumnos.find(checkAlumno);
+		return alumno;
+	}
+};
+$(function() {
+	 //Creamos el modelo con los datos y la conexión al servicio web.
+	 var model = new Model();
+	 model.load();
+	});
+
